4e8c9befaf6a053f76e583439e8f6cba9066b631
169cmharu <habejaron@gmail.com>
2020-02-17 00:10:49 +0800

    Level 5 has been Added

--- app/src/main/java/com/everyday/breadwinner/ChooseLevelActivity.java
+++ app/src/main/java/com/everyday/breadwinner/ChooseLevelActivity.java
@@ -42,6 +42,7 @@ public class ChooseLevelActivity extends AppCompatActivity {
     int day2Status, day2HighScore, day2EarnedStrawberries;
     int day3Status, day3HighScore, day3EarnedStrawberries;
     int day4Status, day4HighScore, day4EarnedStrawberries;
+    int day5Status, day5HighScore, day5EarnedStrawberries;
 
 
     @Override
@@ -331,6 +332,10 @@ public class ChooseLevelActivity extends AppCompatActivity {
         day4Status = dataLevel.getInt("LEVEL_4_STATUS", 0);
         day4HighScore = dataLevel.getInt("LEVEL_4_HIGH_SCORE", 0);
         day4EarnedStrawberries = dataLevel.getInt("LEVEL_4_STRAWBERRIES", 0);
+
+        day5Status = dataLevel.getInt("LEVEL_5_STATUS", 0);
+        day5HighScore = dataLevel.getInt("LEVEL_5_HIGH_SCORE", 0);
+        day5EarnedStrawberries = dataLevel.getInt("LEVEL_5_STRAWBERRIES", 0);
     }
 
     public void selectDay(int day) {
@@ -583,7 +588,7 @@ public class ChooseLevelActivity extends AppCompatActivity {
             // Get Values from string.xml
             String strCurrentDay = getString(R.string.d4);
             String strTargetScore = getString(R.string.td4);
-            String strHighScore = "High Score: " + day3HighScore;
+            String strHighScore = "High Score: " + day4HighScore;
 
             // Set Values
             currentDay.setText(strCurrentDay);
@@ -647,18 +652,41 @@ public class ChooseLevelActivity extends AppCompatActivity {
             Objects.requireNonNull(confirmDayMenu.getWindow()).setFlags(WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE, WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE);
 
             // Edit Values in Confirm Menu According to Day
-            TextView currentDay, targetScore;
+            TextView currentDay, targetScore, highScore, lockMessage;
+            ImageView strawberry1, strawberry2, strawberry3;
             Button startGame;
+
             currentDay = confirmDayMenu.findViewById(R.id.currentDay);
             targetScore = confirmDayMenu.findViewById(R.id.targetScore);
+            highScore = confirmDayMenu.findViewById(R.id.currentScore);
+            strawberry1 = confirmDayMenu.findViewById(R.id.strawberry_1);
+            strawberry2 = confirmDayMenu.findViewById(R.id.strawberry_2);
+            strawberry3 = confirmDayMenu.findViewById(R.id.strawberry_3);
+            lockMessage = confirmDayMenu.findViewById(R.id.lockMessage);
 
             // Get Values from string.xml
             String strCurrentDay = getString(R.string.d5);
             String strTargetScore = getString(R.string.td5);
+            String strHighScore = "High Score: " + day5HighScore;
 
             // Set Values
             currentDay.setText(strCurrentDay);
             targetScore.setText(strTargetScore);
+            highScore.setText(strHighScore);
+
+            // Show Strawberries Earned
+            if (day5EarnedStrawberries == 1) {
+                strawberry1.setImageResource(R.drawable.with_strawberry);
+            }
+            else if (day5EarnedStrawberries == 2) {
+                strawberry1.setImageResource(R.drawable.with_strawberry);
+                strawberry2.setImageResource(R.drawable.with_strawberry);
+            }
+            else if (day5EarnedStrawberries == 3) {
+                strawberry1.setImageResource(R.drawable.with_strawberry);
+                strawberry2.setImageResource(R.drawable.with_strawberry);
+                strawberry3.setImageResource(R.drawable.with_strawberry);
+            }
 
             // Show Dialog
             confirmDayMenu.show();
@@ -676,11 +704,21 @@ public class ChooseLevelActivity extends AppCompatActivity {
             });
 
             startGame = confirmDayMenu.findViewById(R.id.btnStart);
+
+            if (day4Status != 1) {
+                startGame.setVisibility(View.INVISIBLE);
+                lockMessage.setVisibility(View.VISIBLE);
+            }
+            else {
+                startGame.setVisibility(View.VISIBLE);
+            }
+
+
             startGame.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-//                    Intent startDay = new Intent(ChooseLevelActivity.this, Level1.class);
-//                    startActivity(startDay);
+                    Intent startDay = new Intent(ChooseLevelActivity.this, Level5.class);
+                    startActivity(startDay);
                     if (mServ != null) {
                         mServ.pauseMusic();
                     }
---
+++ app/src/main/java/com/everyday/breadwinner/Level6.java
@@ -0,0 +1,13 @@
+package com.everyday.breadwinner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Level6 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
